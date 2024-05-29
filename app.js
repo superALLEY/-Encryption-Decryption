0823af4e1be33c933664f5d334c3e24199fcbb98
Mohamed Ali Inouri <120196622+superALLEY@users.noreply.github.com>
2024-05-29 08:50:41 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,50 @@
+
+
+let text = "Ali";
+let encode_text = window.btoa(text);
+
+let decode_text = window.atob(encode_text);
+
+
+let textarea = document.getElementById("text");
+let result = document.getElementById("result");
+let select = document.getElementById("select");
+
+textarea.addEventListener("input",()=>{
+    make_result();
+});
+select.addEventListener("change",()=>{
+    make_result();
+});
+function make_result(){
+   if(select.value == "decode"){
+    result.value = window.atob(textarea.value) ;
+   }else{
+    result.value = window.btoa(textarea.value) ;
+   }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Coder Shiyar - YouTube : http://youtube.com/codershiyar
